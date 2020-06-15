e5c394d2ffe9ddcc88eb2155a9db76c982d96219
Tim Field <field.tim@gmail.com>
2020-06-15 17:28:37 +1200

    fix onClick overide

--- src/A.tsx
+++ src/A.tsx
@@ -5,14 +5,16 @@ export default function A(props: React.HTMLProps<HTMLAnchorElement>) {
   const setRoute = useSetRoute()
   return (
     <a
+      {...props}
       onClick={ev => {
+        console.log("deep", ev)
+        ev.persist()
         if (props.onClick) {
           props.onClick(ev)
         }
         ev.preventDefault()
         setRoute(props.href)
       }}
-      {...props}
     />
   )
 }
